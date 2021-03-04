926e7d50f16b9cce5b70f0f89daeff04e477b27c
doombubbles <obsidiantoolzg@gmail.com>
2021-03-04 14:37:42 -0800

    Include 2 files

---
+++ BloonsTD6 Mod Helper.Lib/Patches/TitleScreen_Start.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Unity.UI_New.Main;
+using BloonsTD6_Mod_Helper.Api;
+using Harmony;
+using System.Collections.Generic;
+using Assets.Main.Scenes;
+
+namespace BloonsTD6_Mod_Helper.Patches
+{
+    [HarmonyPatch(typeof(TitleScreen), nameof(TitleScreen.Start))]
+    internal class TitleScreen_Start
+    {
+        [HarmonyPostfix]
+        internal static void Postfix()
+        {
+            MelonMain.DoPatchMethods(mod => mod.OnTitleScreen());
+        }
+    }
+}
