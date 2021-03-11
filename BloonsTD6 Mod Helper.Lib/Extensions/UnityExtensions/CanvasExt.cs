f695d17eb044f407e75169aec53548e985ad4cb0
Thomas <thomaspmar@protonmail.com>
2021-03-10 21:16:01 -0800

    Moved GetComponent ext to from Canvas to Component

--- BloonsTD6 Mod Helper.Lib/Extensions/UnityExtensions/CanvasExt.cs
+++ BloonsTD6 Mod Helper.Lib/Extensions/UnityExtensions/CanvasExt.cs
@@ -4,9 +4,6 @@ namespace BloonsTD6_Mod_Helper.Extensions
 {
     public static class CanvasExt
     {
-        public static T GetComponent<T>(this Canvas canvas, string componentPath)
-        {
-            return canvas.transform.Find(componentPath).GetComponent<T>();
-        }
+        
     }
 }
